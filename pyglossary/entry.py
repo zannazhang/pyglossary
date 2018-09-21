1cca7768d384975092fba729cbb0faa3756361a0
Saeed Rasooli <saeed.gnu@gmail.com>
2018-09-21 08:37:42 +0800

    entry.py: define (abstract) class BaseEntry which both Entry and DataEntry inherit from

--- pyglossary/entry.py
+++ pyglossary/entry.py
@@ -9,7 +9,72 @@ from os.path import (
 )
 
 
-class DataEntry(object): # or Resource? FIXME
+class BaseEntry(object):
+	def isData(self):
+		raise NotImplementedError
+
+	def getFileName(self):
+		raise NotImplementedError
+
+	def getData(self):
+		raise NotImplementedError
+
+	def save(self, directory):
+		raise NotImplementedError
+
+	def getWord(self):
+		raise NotImplementedError
+
+	def getWords(self):
+		raise NotImplementedError
+
+	def getDefi(self):
+		raise NotImplementedError
+
+	def getDefis(self):
+		raise NotImplementedError
+
+	def getDefiFormat(self):
+		raise NotImplementedError
+
+	def setDefiFormat(self, defiFormat):
+		raise NotImplementedError
+
+	def detectDefiFormat(self):
+		raise NotImplementedError
+
+	def addAlt(self, alt):
+		raise NotImplementedError
+
+	def editFuncWord(self, func):
+		raise NotImplementedError
+
+	def editFuncDefi(self, func):
+		raise NotImplementedError
+
+	def strip(self):
+		raise NotImplementedError
+
+	def replaceInWord(self, source, target):
+		raise NotImplementedError
+
+	def replaceInDefi(self, source, target):
+		raise NotImplementedError
+
+	def replace(self, source, target):
+		raise NotImplementedError
+
+	def getRaw(self):
+		raise NotImplementedError
+
+	def getEntrySortKey(key=None):
+		raise NotImplementedError
+
+	def getRawEntrySortKey(key=None):
+		raise NotImplementedError
+
+
+class DataEntry(BaseEntry): # or Resource? FIXME
 	def isData(self):
 		return True
 
@@ -103,7 +168,7 @@ class DataEntry(object): # or Resource? FIXME
 		)
 
 
-class Entry(object):
+class Entry(BaseEntry):
 	sep = "|"
 	htmlPattern = re.compile(
 		".*(" + "|".join([
