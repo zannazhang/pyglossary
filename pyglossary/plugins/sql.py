2345ec0b280a55e8b84ea18886f181762606f291
Saeed Rasooli <saeed.gnu@gmail.com>
2018-09-24 00:27:18 +0800

    sql.py: add types and replace single quotes

--- pyglossary/plugins/sql.py
+++ pyglossary/plugins/sql.py
@@ -3,22 +3,22 @@
 from formats_common import *
 
 enable = True
-format = 'Sql'
-description = 'SQL'
-extentions = ['.sql']
+format = "Sql"
+description = "SQL"
+extentions = [".sql"]
 readOptions = []
 writeOptions = [
-	'encoding',  # str
+	"encoding",  # str
 ]
 
 
 def write(
-	glos,
-	filename,
-	encoding='utf-8',
+	glos: GlossaryType,
+	filename: str,
+	encoding: str = "utf-8",
 ):
-	with open(filename, 'w', encoding=encoding) as fp:
+	with open(filename, "w", encoding=encoding) as fp:
 		for line in glos.iterSqlLines(
 			transaction=False,
 		):
-			fp.write(line + '\n')
+			fp.write(line + "\n")
